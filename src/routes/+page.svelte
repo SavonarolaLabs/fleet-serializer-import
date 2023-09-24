08d288fe26fe89ebb5e75cc6c5c7ade4747729e0
Savonarola <x@x.com>
2023-09-24 14:30:14 +0400

    add Price and Seller Address

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,11 @@
     import { buyTx, getBox } from "../erg-contracts/buyFromContract";
 
     let contract:string='';
-    const tokenId = "89963543c7fa6064cf8e5f567740ff060d4a2b94188d1f267db7ae425a574119";
-    const boxId = '1531b63ac1fb483b2c3c49c63c583b6be2f5eb608459eeafaf00c335be0cb2ab'
     let currentTx = ""
-    //const boxId = 'a8fea293382d8a4a2ed4aa2f355c3ed6e4705a3ff0ddf69f011aa7d277ee4c36'
+    const tokenId = "89963543c7fa6064cf8e5f567740ff060d4a2b94188d1f267db7ae425a574119";
+    const boxId = 'a6ca44bf4bc40ee4d9f70ef7f3fc9f080a4e73ffe89edfd2ddcccc78b7a8db6c'
+    const price = 1_000_000_000n
+    const seller = "3Wxa3TmDCRttbDSFxxobU68r9SAPyHcsLwKVwwjGnUDC7yVyYaj3"
 
     onMount(doStuff);
 
@@ -24,7 +25,7 @@
         const me = await ergo.get_change_address();
         const utxos = await ergo.get_utxos();
         const height = await ergo.get_current_height();
-        const tx = await buyTx(boxId, me, tokenId, utxos, height);
+        const tx = await buyTx(boxId, me, tokenId, utxos, height,price,seller);
         //const tx = await getBox(boxId, me, tokenId, utxos, height);
         console.log(tx);
         const signed = await ergo.sign_tx(tx);
@@ -45,7 +46,7 @@
         currentTx = txId
     }
 </script>
+<div>active contract:<a target="_blank" href={`https://testnet.ergoplatform.com/en/addresses/${contract}`}>{contract}</a></div>
 <div><button on:click={sendToken}>sendToken</button></div>
 <div><button on:click={receiveToken}>receiveToken</button></div>
-<div><a target="_blank" href={`https://testnet.ergoplatform.com/en/addresses/${contract}`}>{"https://testnet.ergoplatform.com/en/addresses/"+contract}</a></div>
 <div><a target="_blank" href={`https://testnet.ergoplatform.com/en/transactions/${currentTx}`}>{"https://testnet.ergoplatform.com/en/transactions/"+currentTx}</a></div>
