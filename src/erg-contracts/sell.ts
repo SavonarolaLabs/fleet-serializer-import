9a74178aca5255b823a1857075559d48d997ef2f
Savonarola <x@x.com>
2023-09-24 18:14:08 +0400

    change contract

--- src/erg-contracts/sell.ts
+++ src/erg-contracts/sell.ts
@@ -1,17 +1,13 @@
 const a = `
 SELF.R4[SigmaProp].get || 
-sigmaProp({
-    val priceNanoErg     	= SELF.R5[Long].get         
-    val feeAddress			= SELF.R6[SigmaProp].get.propBytes
-    val feeDenom 			= 100000L
-    val devFee   			= 1000L
-    val fee  			    = (priceNanoErg.toBigInt * devFee.toBigInt) / feeDenom.toBigInt
-    val feePaid				= (fee == 0) || (OUTPUTS(1).value >= fee && OUTPUTS(1).propositionBytes == feeAddress)
+sigmaProp({       
+    val fee  	= (SELF.R5[Long].get.toBigInt * (1000L).toBigInt) / (100000L).toBigInt
+    val feePaid	= (fee == 0) || (OUTPUTS(1).value >= fee && OUTPUTS(1).propositionBytes == SELF.R6[SigmaProp].get.propBytes)
     
     feePaid && 
-    OUTPUTS(0).value-fee>=SELF.R5[Long].get  &&
-    OUTPUTS(0).propositionBytes==SELF.R4[SigmaProp].get.propBytes &&
-    OUTPUTS(0).R4[Coll[Byte]].get==SELF.id
+    OUTPUTS(0).value               >=  SELF.R5[Long].get - fee  &&
+    OUTPUTS(0).propositionBytes    ==  SELF.R4[SigmaProp].get.propBytes &&
+    OUTPUTS(0).R4[Coll[Byte]].get  ==  SELF.id
 })
 
 `
