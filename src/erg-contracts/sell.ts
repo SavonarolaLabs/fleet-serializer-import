0ecc67faf9bf44f11402bc1d37fa6df5a01237e5
Savonarola <x@x.com>
2023-09-24 14:30:53 +0400

    add R5 check to contract

--- src/erg-contracts/sell.ts
+++ src/erg-contracts/sell.ts
@@ -1,6 +1,8 @@
 const a = `
-    SELF.R4[SigmaProp].get
+SELF.R4[SigmaProp].get || sigmaProp (SELF.R5[Long].get>=OUTPUTS(0).value && 
+    SELF.R4[SigmaProp].get.propBytes==OUTPUTS(0).propositionBytes)
 `
+//SELF.R4[SigmaProp].get || 
 /*const b = `{
     val holderPK = SELF.R4[SigmaProp].get
     holderPK  || 
