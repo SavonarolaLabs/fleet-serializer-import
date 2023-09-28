d843949267799d60e8d9998f79767132e8e81e69
c8e4 <c8e4@proton.me>
2023-09-28 17:05:04 +0200

    fix oracle token id check

--- src/erg-contracts/compile.ts
+++ src/erg-contracts/compile.ts
@@ -1,5 +1,5 @@
 import { compile } from "@fleet-sdk/compiler"
-import { ErgoAddress, Network, SColl, SGroupElement, SSigmaProp } from "@fleet-sdk/core"
+import { ErgoAddress, Network, SByte, SColl, SGroupElement, SSigmaProp } from "@fleet-sdk/core"
 import { sell } from "./sell"
 import { hodl } from "./hodl"
 import { first } from "@fleet-sdk/common"
@@ -13,7 +13,8 @@ export function compileHodlContract(devBase58PK: string): string {
     const devAddr = ErgoAddress.fromBase58(devBase58PK)
     const tree = compile(hodl, {
         map: {
-            _contractDevPK: SSigmaProp(SGroupElement(first(devAddr.getPublicKeys()))).toHex()
+            _contractDevPK: SSigmaProp(SGroupElement(first(devAddr.getPublicKeys()))).toHex(),
+            _oraclePoolNFT: SColl(SByte, "0fb1eca4646950743bc5a8c341c16871a0ad9b4077e3b276bf93855d51a042d1").toHex()
         }
     });
     return tree.toAddress(Network.Testnet).toString()
