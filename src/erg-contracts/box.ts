061a9de25ea04df17e18a6a41c482f98e29f7845
c8e4 <c8e4@proton.me>
2023-09-29 12:47:19 +0200

    get boxes by address

--- src/erg-contracts/box.ts
+++ src/erg-contracts/box.ts
@@ -1,7 +1,8 @@
 import { ErgoAddress } from "@fleet-sdk/core";
 
 export async function getContractBoxes(address: string): Promise<any>{
-    const response = await fetch('https://api.ergoplatform.com/api/v1/boxes/byErgoTree/'+ErgoAddress.fromBase58(address).ergoTree);
+    const response = await fetch('https://api.ergoplatform.com/api/v1/boxes/unspent/byAddress/'+address;
+    //const response = await fetch('https://api.ergoplatform.com/api/v1/boxes/byErgoTree/'+ErgoAddress.fromBase58(address).ergoTree);
     const data = await response.json();
     const boxes = data.items.map(nautilusBox);
     return boxes;
