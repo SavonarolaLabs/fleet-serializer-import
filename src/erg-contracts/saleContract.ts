7031c7460ee147dc36410f6ff5f7c0e3d3ec5638
c8e4 <c8e4@proton.me>
2023-10-07 17:29:34 +0200

    buz sell contract

--- src/erg-contracts/saleContract.ts
+++ src/erg-contracts/saleContract.ts
@@ -1,18 +1,17 @@
 export const saleContract = `
 {
-		val priceNanoErg     	= SELF.R4[Long].get         // token sale price in nanoErg
-		val sellerPK 			= SELF.R5[GroupElement].get // Public Key of the token seller
-		val sellerAddress		= SELF.R6[Coll[Byte]].get   // seller receive address
-		val feeAddress			= SELF.R7[Coll[Byte]].get   // ui Fee receive address
+    val priceNanoErg     	= SELF.R4[Long].get         // token sale price in nanoErg
+    val sellerPK 			= SELF.R5[GroupElement].get // Public Key of the token seller
+    val devPK			    = SELF.R6[GroupElement].get // dev Public Key
 
-		val feeDenom 			= 100000L
-        val devFee   			= 1000L
-		val uiFee   			= (priceNanoErg.toBigInt * devFee.toBigInt) / feeDenom.toBigInt
-		val onlyOneBoxSpent		= (OUTPUTS(0).R4[Coll[Byte]].get == SELF.id)
-		val sellerHappy			= (OUTPUTS(0).value >= priceNanoErg - uiFee) && (OUTPUTS(0).propositionBytes == sellerAddress)
-		val feePaid				= (uiFee == 0) ||
-								  (OUTPUTS(1).value >= uiFee && OUTPUTS(1).propositionBytes == feeAddress)
-		sigmaProp(onlyOneBoxSpent && sellerHappy && feePaid) || proveDlog(sellerPK)
+    val feeDenom 			= 100000L
+    val feeNum   			= 1000L                     // 1% fee
+    val feeInNanoErg   		= (priceNanoErg.toBigInt * feeNum.toBigInt) / feeDenom.toBigInt
+    val onlyOneBoxSpent		= (OUTPUTS(0).R4[Coll[Byte]].get == SELF.id)
+    val sellerHappy			= (OUTPUTS(0).value >= priceNanoErg - feeInNanoErg) && (OUTPUTS(0).propositionBytes == sellerPK.propBytes)
+    val feePaid				= (feeInNanoErg == 0) ||
+                              (OUTPUTS(1).value >= feeInNanoErg && OUTPUTS(1).propositionBytes == devPK.propBytes)
+    sigmaProp(onlyOneBoxSpent && sellerHappy && feePaid) || proveDlog(sellerPK)
 }
 `
 
