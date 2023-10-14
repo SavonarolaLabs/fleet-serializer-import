d3a8981c8fb356a30c8f645e64bf20b57c9a149c
Savonarola <x@x.com>
2023-10-14 18:36:59 +0400

    Compile HodlErg3Box

--- src/erg-contracts/compile.ts
+++ src/erg-contracts/compile.ts
@@ -2,6 +2,7 @@ import { compile } from "@fleet-sdk/compiler"
 import { ErgoAddress, Network, SByte, SColl, SGroupElement, SSigmaProp } from "@fleet-sdk/core"
 import { sell } from "./sell_contract/sell"
 import { hodl } from "./hodl_contract/hodl"
+import { hodlErg3 } from "./hodl_contract/hodl_hodlerg3"
 import { first } from "@fleet-sdk/common"
 import { sellFee } from "./sell_fee_contract/sell_fee"
 
@@ -25,6 +26,23 @@ export function compileHodlContract(devBase58PK: string): string {
     });
     return tree.toAddress(Network.Mainnet).toString()
 }
+
+
+export function compileHodlErg3Contract(devBase58PK: string): string {
+    const devAddr = ErgoAddress.fromBase58(devBase58PK)
+    const tree = compile(hodlErg3, {
+        map: {
+            _contractDevPK: SSigmaProp(SGroupElement(first(devAddr.getPublicKeys()))).toHex(),
+            _oraclePoolNFT: SColl(SByte, "011d3364de07e5a26f0c4eef0852cddb387039a921b7154ef3cab22c6eda887f").toHex()
+        }
+    });
+    return tree.toAddress(Network.Mainnet).toString()
+}
+
+
+
+
+
 //011d3364de07e5a26f0c4eef0852cddb387039a921b7154ef3cab22c6eda887f //new
 //0fb1eca4646950743bc5a8c341c16871a0ad9b4077e3b276bf93855d51a042d1 // old 
 
